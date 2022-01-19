920dfcd1978b6266227aaf8214468d02f9b76cea
Miguel Angel Bolaños Alvarado <migb4@hotmail.com>
2022-01-18 20:32:20 -0600

    lab-03

---
+++ MobileApp/Views/ToDosPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace MobileApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ToDosPage : ContentPage
+    {
+        public ObservableCollection<object> ToDos { get; set; } = new ObservableCollection<object>();
+        public ToDosPage()
+        {
+            Title = "My to Dos";
+
+            InitializeComponent();
+
+            ToDos.Add("Buy Cookies");
+            ToDos.Add("testc 2");
+
+            BindableLayout.SetItemsSource(ItemsContainer, ToDos);
+
+            Root.Children.Add(new Button { Text = "Add",
+                Command = new Command(() => AddItem() ),
+                //VerticalOptions
+            });
+            
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ToDos.Add(DateTime.Now);
+
+        }
+
+        private void AddItem() {
+            ToDos.Add(DateTime.Now);
+        }
+    }
+}
